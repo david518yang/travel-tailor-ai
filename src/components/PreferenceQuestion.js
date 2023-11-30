4f68d0bbd595e471a8553bffb61068c2dcdec496
david518yang <davidkunheeyang@gmail.com>
2023-11-29 18:14:47 -0800

    added preferences page

--- src/components/PreferenceQuestion.js
+++ src/components/PreferenceQuestion.js
@@ -5,14 +5,16 @@ class PreferenceQuestion extends React.Component {
     const { question, answerChoices } = this.props
 
     return (
-      <div className="p-6">
-        <h3>{question}</h3>
-        {answerChoices.map((choice, index) => (
-          <div key={index} className="flex items-center mb-6">
-            <input type="checkbox" id={`choice-${index}`} className="mr-2" />
-            <label htmlFor={`choice-${index}`}>{choice}</label>
-          </div>
-        ))}
+      <div className="p-3 space-y-5 -my-3">
+        <h3 className="font-bold">{question}</h3>
+        <div className="flex flex-wrap -mx-2">
+          {answerChoices.map((choice, index) => (
+            <div key={index} className="flex items-center mb-2 px-2">
+              <input type="checkbox" id={`choice-${index}`} className="mr-2" />
+              <label htmlFor={`choice-${index}`}>{choice}</label>
+            </div>
+          ))}
+        </div>
       </div>
     )
   }
--- src/pages/Preferences.js
+++ src/pages/Preferences.js
@@ -1,23 +1,131 @@
 import React from 'react'
 import PreferenceQuestion from '../components/PreferenceQuestion'
+import GenerateRecommendationsButton from '../components/GenerateRecommendationsButton'
 
 export default function Preferences() {
   return (
-    <div>
-      <h1>Enter Preferences</h1>
-      <PreferenceQuestion
-        question="What are you looking to achieve on your trip?"
-        answerChoices={[
-          'Beach Relaxation',
-          'Culinary Delights',
-          'Festivities & Events',
-          'Cultural Experiences',
-          'Nature Escape',
-          'Luxury Retreat',
-          'Urban Discovery',
-          'Other'
-        ]}
-      />
+    <div className="grid grid-cols-12 gap-5 px-5 py-20 bg-sky-200">
+      <div className="col-start-2 col-end-12 bg-amber-100 p-10 shadow-lg rounded-3xl">
+        <h1 className="text-4xl mb-6 text-center font-lato">Welcome to Your Personalized Travel Experience</h1>
+        <p className="mb-10 text-center font-lato">
+          Let's tailor your next adventure! Share a few preferences, and we'll curate destinations just for you. <br />
+          Your journey to unforgettable places begins here.
+        </p>
+        <div className="grid grid-cols-12 gap-5">
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="What are you looking to achieve on your trip?"
+              answerChoices={[
+                '🏖️ Beach Relaxation',
+                '🍽️ Culinary Delights',
+                '🎪 Festivities & Events',
+                '🏺 Cultural Experiences',
+                '🌲 Nature Escape',
+                '🌟 Luxury Retreat',
+                '🌆 Urban Discovery',
+                'Other'
+              ]}
+            />
+          </div>
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="Which climate zones do you love for vacationing?"
+              answerChoices={[
+                '🏜️ Desert',
+                '❄️ Arctic',
+                '🌦️ Temperate',
+                '🌴 Tropical',
+                '☀️ Sunny',
+                '🏔️ Alpine',
+                '🌁️ Foggy',
+                '💧 Humid',
+                '🌊 Marine',
+                '🌧️ Rainy',
+                'Other'
+              ]}
+            />
+          </div>
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="What activities do you seek out while traveling?"
+              answerChoices={[
+                '🏯 Sightseeing',
+                '🐘 Wildlife Encounters',
+                '🤿 Snorkeling',
+                '🍽️ Gourmet Dining',
+                '🥾 Hiking',
+                '🪂 Thrill-Seeking',
+                '🎶 Live Performances',
+                '📸 Photography',
+                '🛍️ Shopping',
+                'Other'
+              ]}
+            />
+          </div>
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="Which cultural aspects are you most interested in exploring?"
+              answerChoices={[
+                '🎭 Performing Arts',
+                '🖼️ Art Galleries',
+                '🏰 Architecture',
+                '🎶 Music',
+                '📜 History',
+                '💃 Dance',
+                '🍜 Cuisine',
+                '🎉 Festivals',
+                '🏛️ Museums',
+                'Other'
+              ]}
+            />
+          </div>
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="What dining experiences do you prefer while on vacation?"
+              answerChoices={[
+                '🍽️ Local Restaurants',
+                '🌟 Gourmet Dining',
+                '🛒 Marketplaces',
+                '🍢 Street Food',
+                '🥗 Healthy Options',
+                '☕ Cafés and Bakeries',
+                '🍳 Cook-Your-Own',
+                '🎪 Food Festivals',
+                'Other'
+              ]}
+            />
+          </div>
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="How do you prefer to get around during your travels?"
+              answerChoices={[
+                '🚆 Trains',
+                '🚗 Driving',
+                '🚌 Public Transit',
+                '🚖 Taxi',
+                '🛥️ Ferries/Boats',
+                '🛴 Electric Scooters',
+                '🚲 Biking',
+                '🚶 Walking',
+                'Other'
+              ]}
+            />
+          </div>
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="What's your spending style for travel accommodations and activities?"
+              answerChoices={['💵 Budget', '🏨 Comfort', '💳 Mid-Range', '🍸 Upscale', '🛎️ Deluxe', '💎 Exclusive']}
+            />
+          </div>
+          <div className="col-start-2 col-end-12 bg-gray-200 p-5 rounded-3xl">
+            <PreferenceQuestion
+              question="For your upcoming travels, how do you envision your ideal travel party?"
+              answerChoices={['👤 Solo', '👥 Couple', '🧑‍🤝‍🧑 Small Group', '👨‍👩‍👧‍👦 Family', '🚌 Large Tour Group', 'Other']}
+            />
+          </div>
+        </div>
+        <GenerateRecommendationsButton />
+      </div>
     </div>
   )
 }
